e568120614c531ab602c513324f008bb204d222b
Srgr0 <66754887+Srgr0@users.noreply.github.com>
2023-07-26 17:36:07 +0900

    v1.1.0

--- chrome/background.js
+++ chrome/background.js
@@ -1,46 +1,51 @@
-// Function to convert URLs of a specific format
-function convertUrl(url) {
-    const match = url.match(/@[^\s@]*@[^\s@]*/);
-    if (match) {
-        const usernameDomain = match[0].substring(1).split('@');
-        return `https://${usernameDomain[1]}/@${usernameDomain[0]}`;
-    }
-    return url;
-}
-
 // Listen for a click on the browser action icon
 chrome.action.onClicked.addListener((tab) => {
-  // Get the current URL
-  var currentUrl = convertUrl(tab.url);
+    // Get the site domain and API key from chrome.storage
+    chrome.storage.local.get(['siteDomain', 'apiKey'], function(data) {
+        var siteDomain = data.siteDomain;
+        var apiKey = data.apiKey;
 
-  // Get the site domain and API key from storage
-  chrome.storage.local.get(['siteDomain', 'apiKey'], function(data) {
-      var siteDomain = data.siteDomain;
-      var apiKey = data.apiKey;
-
-      // Make a POST request to the site domain
-      fetch(`${siteDomain}/api/ap/show`, {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({uri: currentUrl, i: apiKey}),
-      })
-      .then(response => response.json())
-      .then(data => {
-          // Check the type of the response
-          if (data.type === 'User') {
-              // Open a new tab with the user's URL
-              var userUrl = `${siteDomain}/@${data.object.username}@${data.object.host}`;
-              chrome.tabs.create({ url: userUrl });
-          } else if (data.type === 'Note') {
-              // Open a new tab with the note's URL
-              var noteUrl = `${siteDomain}/notes/${data.object.id}`;
-              chrome.tabs.create({ url: noteUrl });
-          }
-      })
-      .catch((error) => {
-          console.error('Error:', error);
-      });
-  });
+        // Parse the current URL
+        const currentUrl = new URL(tab.url);
+        const currentDomain = currentUrl.hostname;
+        // 0:,1:notes,2:xxxxxx or 0:,1:@srgr0
+        const currentPath = currentUrl.pathname.split("/");
+        // Check the path of the URL
+        if (currentPath[1] === "notes") {
+            if (currentDomain !== siteDomain) {
+                // Make a POST request to the site domain
+                fetch(`${siteDomain}/api/ap/show`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({uri: tab.url, i: apiKey}),
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.type === 'Note') {
+                        // Open a new tab with the note's URL
+                        var noteUrl = `${siteDomain}/notes/${data.object.id}`;
+                        chrome.tabs.create({ url: noteUrl });
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+            }
+        } else {
+            if (currentDomain !== siteDomain) {
+                // Check if the username has a domain
+                // 0:,1:srgr0,2:misskey.srgr0.com or 0:,1:srgr0
+                const userParts = currentPath[1].split("@");
+                var userUrl;
+                if (userParts.length === 3) {
+                    userUrl = `${siteDomain}/@${userParts[1]}@${userParts[2]}`;
+                } else {
+                    userUrl = `${siteDomain}/@${userParts[1]}@${currentDomain}`;
+                }
+                chrome.tabs.create({ url: userUrl });
+            }
+        }
+    });
 });
